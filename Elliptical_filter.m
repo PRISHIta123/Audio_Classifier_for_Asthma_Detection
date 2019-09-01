3f2e67854dfff743797e79c8df6c9ab1b25f5eea
Prishita Ray <30746562+PRISHIta123@users.noreply.github.com>
2019-09-01 23:41:56 +0530

    Update Elliptical_filter.m

--- Elliptical_filter.m
+++ Elliptical_filter.m
@@ -1,1 +1,11 @@
-
+ws = [0.3*pi 0.75*pi] %Stopband edge frequency
+wp = [0.4*pi 0.6*pi] %Passband edge frequency
+Rp = 0.5; %Passband ripple in dB
+As = 20; %Stopband attenuation in dB
+[N,wn] = ellipord(wp/pi,ws/pi,Rp,As);
+[b,a] = ellip(N,Rp,As,wn);
+[db,mag,pha,grd,w] = freqz_m(b,a);
+plot(w*8000/2/pi,db);
+axis([500 3500 -22 1]);
+xlabel('frequency (Hz)'); ylabel('decibels'); title('Magnitude
+Response of Elliptic Filter');
