b1b398c136209909e9111e69114605aa43a3f7ca
Prishita Ray <30746562+PRISHIta123@users.noreply.github.com>
2019-09-05 14:59:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import chebyshev_and_elliptic_filters
 from mlabwrap import mlab
 
-mlab.path(mlab.path(),path_to_directory)
+mlab.path(mlab.path(),path_to_matlab_func_directory)
 fs=mlab.freqCheck()
 chebylowf=chebyshev_and_elliptic_filters.IIR2Filter(self,order,cutoff,'lowpass','cheby2',1,1,fs)
 chebyhighf=chebyshev_and_elliptic_filters.IIR2Filter(self,order,cutoff,'highpass','cheby2',1,1,fs)
