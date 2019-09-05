5ae4612695b245c5f52ca0e253cd7313b10849f2
Prishita Ray <30746562+PRISHIta123@users.noreply.github.com>
2019-09-05 14:57:52 +0530

    Update and rename pitchSound.m to freqCheck.m

--- pitchSound.m
+++
@@ -1,9 +0,0 @@
-[audioIn,fs] = audioread('Name of File saved in Desktop/Folder');
-[f0,idx] = pitch(audioIn,fs);
-figure
-plot(idx,f0)
-ylabel('Pitch (Hz)')
-xlabel('Sample Number')
-a=max(f0);
-b=min(f0);
-disp(a);disp(b)
