6e73bd53361848b7d3ef0bfc65c916469e5db39c
Prishita Ray <30746562+PRISHIta123@users.noreply.github.com>
2019-09-17 09:48:01 +0530

    Update freqCheck.m

--- freqCheck.m
+++ freqCheck.m
@@ -1,7 +1,8 @@
 [audioIn,fs] = audioread('G:\respiratory-sound-database\Respiratory_Sound_Database\Respiratory_Sound_Database\audio_and_txt_files\107_2b5_Ar_mc_AKGC417L.wav');
 fs
 spectrum=pwelch(audioIn); %pitch function not working in Matlab 2017
-plot(10*log10(spectrum))
-drawnow;
-sound(audioIn,fs);
 %highest recorded frequency for test audio file is 44100 Hz
+N= length(audioIn);
+slength=N/fs;
+t=linspace(0,N/fs,N);
+plot(t,audioIn);
