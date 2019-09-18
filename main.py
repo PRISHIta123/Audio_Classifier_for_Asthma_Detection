0a12be499601436a9fe91613244a670a5b646346
Prishita Ray <30746562+PRISHIta123@users.noreply.github.com>
2019-09-18 21:54:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import chebyshev_and_elliptic_filters
-from mlabwrap import mlab
+import wave
+import numpy as np
+import pandas as pd
 
-mlab.path(mlab.path(),path_to_matlab_func_directory)
-fs=mlab.freqCheck()
+fs=44100
+order=20
+cutoff=0.25*fs
 chebylowf=chebyshev_and_elliptic_filters.IIR2Filter(self,order,cutoff,'lowpass','cheby2',1,1,fs)
 chebyhighf=chebyshev_and_elliptic_filters.IIR2Filter(self,order,cutoff,'highpass','cheby2',1,1,fs)
 ellipf=chebyshev_and_elliptic_filters.IIR2Filter(self,order,cutoff,'lowpass','elliptic',1,1,fs)
 
+audio_filepaths=pd.read_csv("F://audio_paths.csv",header=None)
+
+ar=audio_filepaths.values
+paths=ar[0,:]
+
+for x in paths:
+  file=wave.read(x,'r')
+  
+  
+  
